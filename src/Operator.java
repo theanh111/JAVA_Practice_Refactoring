769d80e62da6b1172ca6d4d115b3e80190a080d5
anh.tran(c0720i1) <69283798+theanh111@users.noreply.github.com>
2020-09-18 10:52:46 +0700

    thuc hanh Refactoring

---
+++ src/Operator.java
@@ -0,0 +1,6 @@
+public class Operator {
+    public static final char ADDITION = '+';
+    public static final char SUBTRACTION = '-';
+    public static final char MULTIPLICATION = '*';
+    public static final char DIVISION = '/';
+}
